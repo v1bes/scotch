9f855cb7130be5f2c33781f1f9ca4e2b34c7bee1
marcolilli <github@einmarco.com>
2018-06-05 21:06:04 +0200

    basic gulpfile for development

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,24 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-
-const paths = {
-    styles: {
-        src: 'styles/**/*.sass',
-        dest: 'styles/css/'
-    }
-};
+const watch = require('gulp-watch');
 
 
 /*
- * Define tasks
+ * You can eigher use 'gulp sass' to compile your sass to css or
+ * 'gulp watch' to start a watcher, which will automatically compile
+ * when you make changes to any sass file.
  */
 
-//sass to css
-gulp.task('sass', function(){
-    return gulp.src('./sass/**/*.sass')
+// Sass to css
+gulp.task('sass', () => {
+    return gulp.src('kit/css/main.sass')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('kit/css/'));
 });
 
 
-
+// Watcher
+gulp.task('watch', () => {
+    gulp.watch('kit/**/*.sass', ['sass']);
+});
